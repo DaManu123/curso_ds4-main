895401add30011fcd2474b8d1a02add44b445ab8
DaManu123 <manumr_04@hotmail.com>
2025-02-25 10:58:42 -0700

    funciones de game juego

--- games/Game.py
+++ games/Game.py
@@ -1,6 +1,9 @@
+from random import choice
+
 from Athlete import Athlete
 from Sport import Sport
 from Team import Team
+from random import choice
 
 class Game:
     '''Class Game: Juego entre dos equipos'''
@@ -20,16 +23,35 @@ class Game:
 
     def play(self):
         '''Juego simulado entre equipos'''
-        for s in self.sports_dict.values():
-            print(s)
+        league = self.A.sport.league
+        points = self.sports_dict[league]
+        a = choice(points)
+        b = choice(points)
+        self.score[self.A.name] = a
+        self.score[self.B.name] = b
 
+    def __str__(self) -> str:
+        '''Metodo para mostrar clase como string'''
+        return f"""Game: {self.A.name:20s}: {self.score[self.A.name]:3d} - {self.score[self.B.name]:3d}: {self.B.name:20s}"""
+
+    def __repr__(self) -> str:
+        '''Metodo para representar la clase como string'''
+        return f"Game(A={repr(self.A)}, B={repr(self.B)})"
+
+    def to_json(self) -> dict:
+        '''Metodo para representar la clase como diccionario'''
+        return {"A":self.A.to_json(), "B":self.B.to_json(),"score":self.score}
 if __name__ == '__main__':
     dt = ['Jordan','Johnson','Pipen','Bird','Kobe']
     cz = ['Bjovik','Czack','Pfeizer','Leonard','Kempfe']
     players_a = [Athlete(x) for x in dt]
     players_b = [Athlete(x) for x in cz]
-    basketball = Sport("NBA",5,"DreamTeam")
-    t = Team("Dream Team",basketball)
-    c = Team("Czeck Republic",basketball)
+    basketball = Sport("DreamTeam",5,"NBA")
+    t = Team("Dream Team",basketball,players_a)
+    c = Team("Czeck Republic",basketball,players_b)
     game = Game(t, c)
     game.play()
+    print(game)
+    print(repr(game))
+    print("----")
+    print(game.to_json())
