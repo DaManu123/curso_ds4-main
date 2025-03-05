1598c44a468a65c57f2f4ce4aca610e19dcf1146
DaManu123 <manumr_04@hotmail.com>
2025-03-05 10:51:06 -0700

    introduccion a funcionespy y app de web html

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")  # home or root of website
+def index():
+    return '''<html>
+                <head>
+                <title>HELLO WORLD</title>
+                </head>
+                <body>
+                    <h1>Hello World</h1>
+                    <p>Go to <a href="/about">about</a></p>
+                </body>
+            </html>'''
+
+
+@app.route("/about")  # info about this site
+def about():
+    return '''<html>
+                <head>
+                <title>About this page</title>
+                </head>
+                <body>Everything about this website. Back to <a href="/">Hello World</a>
+                </body>
+            </html>'''
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
